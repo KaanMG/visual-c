b431df6101cdf54ad070343cdc1d1585c8d08066
Kaan <gencdogankaan.2004@gmail.com>
2023-11-10 10:43:25 +0300

    update ise_alim

--- ise_alim.c
+++ ise_alim.c
@@ -2,66 +2,66 @@
 
 main(){
     int yas;
+    char surucuBelgesi[50],mezuniyet[50],surucuBelgesiTipi[50],cevap[50],cevap1[50];
 
     printf("Yasinizi giriniz = ");
     scanf("%d", &yas);
 
     if(yas <= 40){
-        char[50] surucuBelgesi, mezuniyet;
         printf("Universite mezunu musunuz ? (Y/N)");
+        scanf("%c",&mezuniyet);
         if(mezuniyet == "Y"){
             printf("Surucu belgeniz var mi ? (Y/N)");
             scanf("%c", &surucuBelgesi);
             if(surucuBelgesi == "Y"){
-                char[50] surucuBelgesiTipi;
                 printf("Surucu belgenizin tipini giriniz = ");
                 scanf("%c", &surucuBelgesiTipi);
                 if(surucuBelgesiTipi == "C1" || "C1E" || "C" || "CE"){
                     printf("Tebrikler sartlarimizi karsiliyorsunuz ise alindiniz");
                 }
                 else if(surucuBelgesiTipi == "A" || "A2"){
-                    char[50] cevap;
                     printf("Basvuru sartlarimizi saglamiyorsunuz ama Motokurye sartlarimizi sagliyorsunuz \nMotokurye olarak ise baslamak ister misiniz ? (Y/N)");
                     scanf("%c"), &cevap;
-                    if(cevap == "Y"){
+                    if(cevap == "Y")
+                    {
                         printf("Tebrikler Motokurye olarak ise alindiniz");
                     }
-                    else(){
+                    else
+                    {
                         printf("Iyi günler dileriz");
                     }
 
                 }
                 else if(surucuBelgesiTipi == "D"){
-                    char[50] cevap1;
                     printf("Basvuru sartlarimizi saglamiyorsunuz ama Servis Soforlugu sartlarimizi sagliyorsunuz \nServis Soforu olarak ise baslamak ister misiniz ? (Y/N)");
-                    scanf("%d", &cevap1);
+                    scanf("%c", &cevap1);
                     if(cevap1 == "Y"){
                         printf("Tebrikler Servis Soforu olarak ise alindiniz");
                     }
-                    else(){
+                    else{
                         printf("Iyi gunler dileriz");
                     }
                     
                 }
-                else(){
-                    printf("Iyi gunler dileriz")
+                else{
+                    printf("Iyi gunler dileriz");
                 }
 
 
                                      
             }
 
-            else(){
+            else{
                 printf("Uzgunuz sartlarimizi karsilamiyorsunuz \nIyi gunler dileriz");
             }
 
         }
         
-        else(){
+        else{
             printf("Uzgunuz sartlarimizi karsilamiyorsunuz \nIyi gunler dileriz");
         }
     }
-    else(){
+    else{
         printf("Uzgunuz sartlarimizi karsilamiyorsunuz \nIyi gunler dileriz");
     }
 }
