4ebaf7c243077c40a4b22f8ea8e8b6178ba54fac
Kaan <gencdogankaan.2004@gmail.com>
2024-01-19 19:51:02 +0300

    create
    new

---
+++ series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int factorial(int number)
+{
+    int i,factorial = 1;
+    for (i = 1; i <= (number*2); i++)
+    {
+        factorial *= i;
+        return factorial;
+    }
+}
+
+double main()
+{
+    int n,k;
+    double process = 0;
+    printf("Enter of the value n = ");
+    scanf("%d",&n);
+
+    for (k=1; k<=n; k++)
+    {
+        process += (double)(pow(5,k))/(factorial(k));
+    }
+    printf("%lf",process);
+}
