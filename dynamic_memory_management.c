5e9d9ac312a5ff5a4b578da5fe6776be2fd85264
Kaan <gencdogankaan.2004@gmail.com>
2024-03-21 15:28:12 +0300

    update and new file

---
+++ dynamic_memory_management.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    malloc = The program allocates a certain memory space during runtime.
+    calloc = The program allocates a certain memory space during runtime and initializes this space to zero.
+    realloc = It is used to resize the memory allocated with malloc or calloc.
+    free = It releases the allocated memory space.
+    */
+
+   
+}
--- sait_hoca_ders.c
+++ sait_hoca_ders.c
@@ -5,22 +5,46 @@
 
 int main()
 {
+    
+    /*
     float number;
     
     srand(time(NULL));
     number = rand();
-    /*
+    
     printf("Enter the number = ");
     scanf("%f",&number);
-    */
-    /*
+    
+    
     if(number < 0)
     {
         number *= -1.0;
     }
-    */
+    
 
     printf("%f",round(sqrt(abs(number))));
 
     return 0;
+    */
+
+   int i = 10, j = 5;
+   int *p, *q;
+
+   p = &i;
+   q = &j;
+
+   j = *p + *q;
+
+    printf("i = %d, j = %d\n",i ,j);
+    printf("*p = %d *q = %d\n",*p, *q);
+    
+    /*
+    printf("address of i = %d\n",&i);
+    printf("address of i = %d\n",p);
+    printf("address of i = %p\n",p);
+    printf("address of i = %x\n",p);
+    printf("address of i = %X\n",p);
+    */
+   
+
 }
